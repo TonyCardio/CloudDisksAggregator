8f3dbdb443f4abbdd3407bf30c7205ab2f41f588
Ilya_Shumilin <i.sh-vk@yandex.ru>
2021-04-20 00:52:38 +0500

    feature:     - add Auth Control

---
+++ CloudDisksAggregator/AuthAppData/DropboxAuthData.cs
@@ -0,0 +1,19 @@
+﻿using Dropbox.Api;
+
+namespace CloudDisksAggregator.AuthAppData
+{
+    public class DropboxAuthData : IAuthData
+    {
+        public string AuthUrl { get; }
+
+        public string RedirectUrl { get; }
+
+        public string AppId { get; } = "6dmbqyqong7h511";
+
+        public DropboxAuthData()
+        {
+            RedirectUrl = "https://oauth.yandex.ru/verification_code";
+            AuthUrl = DropboxOAuth2Helper.GetAuthorizeUri(OAuthResponseType.Token, AppId, RedirectUrl).AbsoluteUri;
+        }
+    }
+}
---
+++ CloudDisksAggregator/ViewControls/AuthCloudControl.cs
@@ -0,0 +1,43 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using CloudDisksAggregator.AuthAppData;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CloudDisksAggregator.ViewControls
+{
+    public partial class AuthCloudControl : UserControl
+    {
+        private readonly IAuthData authData;
+        public event Action<UserControl, string> AuthSucceeded;
+        private readonly ChromiumWebBrowser browser;
+        private readonly Regex tokenPattern = new Regex("access_token=(.+)&[t|e]");
+
+        public AuthCloudControl(IAuthData authData)
+        {
+            Name = "AuthCloudControl";
+            InitializeComponent();
+            this.authData = authData;
+            Dock = DockStyle.Fill;
+            browser = new ChromiumWebBrowser(authData.AuthUrl);
+            Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+            browser.AddressChanged += ParseToken;
+            Disposed += AuthCloudControl_Disposed;
+        }
+
+        private void ParseToken(object sender, AddressChangedEventArgs e)
+        {
+            var token = tokenPattern.Match(e.Address);
+            if (token.Success)
+                AuthSucceeded(this, token.Groups[1].Value);
+        }
+
+        private void AuthCloudControl_Disposed(object sender, EventArgs e)
+        {
+            browser.Dispose();
+            Dispose();
+        }
+    }
+}
