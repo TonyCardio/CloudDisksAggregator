6953a977e6c409767b54436ffce2b8999d8e7400
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2021-04-20 20:53:02 +0500

    feature:     - add app ui mvp

--- CloudDisksAggregator/MainForm.Designer.cs
+++ CloudDisksAggregator/MainForm.Designer.cs
@@ -29,12 +29,324 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sideMenuPanel = new System.Windows.Forms.Panel();
+            this.helpBtn = new System.Windows.Forms.Button();
+            this.dropOptionPanel = new System.Windows.Forms.Panel();
+            this.dropChangeBtn = new System.Windows.Forms.Button();
+            this.dropDeleteBtn = new System.Windows.Forms.Button();
+            this.dropBtn = new System.Windows.Forms.Button();
+            this.yaOptionPanel = new System.Windows.Forms.Panel();
+            this.yaChangeBtn = new System.Windows.Forms.Button();
+            this.yaDeleteBtn = new System.Windows.Forms.Button();
+            this.yaDiskBtn = new System.Windows.Forms.Button();
+            this.addNewBtn = new System.Windows.Forms.Button();
+            this.logoPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.controlPanel = new System.Windows.Forms.Panel();
+            this.addDiskSelectPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.addYaDiskBtn = new System.Windows.Forms.Button();
+            this.addDropboxBtn = new System.Windows.Forms.Button();
+            this.sideMenuPanel.SuspendLayout();
+            this.dropOptionPanel.SuspendLayout();
+            this.yaOptionPanel.SuspendLayout();
+            this.logoPanel.SuspendLayout();
+            this.controlPanel.SuspendLayout();
+            this.addDiskSelectPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sideMenuPanel
+            // 
+            this.sideMenuPanel.AutoScroll = true;
+            this.sideMenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.sideMenuPanel.Controls.Add(this.helpBtn);
+            this.sideMenuPanel.Controls.Add(this.dropOptionPanel);
+            this.sideMenuPanel.Controls.Add(this.dropBtn);
+            this.sideMenuPanel.Controls.Add(this.yaOptionPanel);
+            this.sideMenuPanel.Controls.Add(this.yaDiskBtn);
+            this.sideMenuPanel.Controls.Add(this.addNewBtn);
+            this.sideMenuPanel.Controls.Add(this.logoPanel);
+            this.sideMenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sideMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.sideMenuPanel.Name = "sideMenuPanel";
+            this.sideMenuPanel.Size = new System.Drawing.Size(200, 761);
+            this.sideMenuPanel.TabIndex = 0;
+            // 
+            // helpBtn
+            // 
+            this.helpBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.helpBtn.FlatAppearance.BorderSize = 0;
+            this.helpBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(20)))), ((int)(((byte)(0)))));
+            this.helpBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.helpBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.helpBtn.Location = new System.Drawing.Point(0, 395);
+            this.helpBtn.Name = "helpBtn";
+            this.helpBtn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.helpBtn.Size = new System.Drawing.Size(200, 45);
+            this.helpBtn.TabIndex = 6;
+            this.helpBtn.TabStop = false;
+            this.helpBtn.Text = "Help";
+            this.helpBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.helpBtn.UseVisualStyleBackColor = true;
+            // 
+            // dropOptionPanel
+            // 
+            this.dropOptionPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.dropOptionPanel.Controls.Add(this.dropChangeBtn);
+            this.dropOptionPanel.Controls.Add(this.dropDeleteBtn);
+            this.dropOptionPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dropOptionPanel.Location = new System.Drawing.Point(0, 315);
+            this.dropOptionPanel.Name = "dropOptionPanel";
+            this.dropOptionPanel.Size = new System.Drawing.Size(200, 80);
+            this.dropOptionPanel.TabIndex = 5;
+            this.dropOptionPanel.Visible = false;
+            // 
+            // dropChangeBtn
+            // 
+            this.dropChangeBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dropChangeBtn.FlatAppearance.BorderSize = 0;
+            this.dropChangeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dropChangeBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.dropChangeBtn.Location = new System.Drawing.Point(0, 40);
+            this.dropChangeBtn.Name = "dropChangeBtn";
+            this.dropChangeBtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.dropChangeBtn.Size = new System.Drawing.Size(200, 40);
+            this.dropChangeBtn.TabIndex = 1;
+            this.dropChangeBtn.Text = "Change name";
+            this.dropChangeBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dropChangeBtn.UseVisualStyleBackColor = true;
+            // 
+            // dropDeleteBtn
+            // 
+            this.dropDeleteBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dropDeleteBtn.FlatAppearance.BorderSize = 0;
+            this.dropDeleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dropDeleteBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.dropDeleteBtn.Location = new System.Drawing.Point(0, 0);
+            this.dropDeleteBtn.Name = "dropDeleteBtn";
+            this.dropDeleteBtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.dropDeleteBtn.Size = new System.Drawing.Size(200, 40);
+            this.dropDeleteBtn.TabIndex = 0;
+            this.dropDeleteBtn.Text = "Delete";
+            this.dropDeleteBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dropDeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // dropBtn
+            // 
+            this.dropBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dropBtn.FlatAppearance.BorderSize = 0;
+            this.dropBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(20)))), ((int)(((byte)(0)))));
+            this.dropBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dropBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dropBtn.Location = new System.Drawing.Point(0, 270);
+            this.dropBtn.Name = "dropBtn";
+            this.dropBtn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.dropBtn.Size = new System.Drawing.Size(200, 45);
+            this.dropBtn.TabIndex = 4;
+            this.dropBtn.TabStop = false;
+            this.dropBtn.Text = "Dropbox";
+            this.dropBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dropBtn.UseVisualStyleBackColor = true;
+            this.dropBtn.Click += new System.EventHandler(this.dropBtn_Click);
+            // 
+            // yaOptionPanel
+            // 
+            this.yaOptionPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.yaOptionPanel.Controls.Add(this.yaChangeBtn);
+            this.yaOptionPanel.Controls.Add(this.yaDeleteBtn);
+            this.yaOptionPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.yaOptionPanel.Location = new System.Drawing.Point(0, 190);
+            this.yaOptionPanel.Name = "yaOptionPanel";
+            this.yaOptionPanel.Size = new System.Drawing.Size(200, 80);
+            this.yaOptionPanel.TabIndex = 3;
+            this.yaOptionPanel.Visible = false;
+            // 
+            // yaChangeBtn
+            // 
+            this.yaChangeBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.yaChangeBtn.FlatAppearance.BorderSize = 0;
+            this.yaChangeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yaChangeBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.yaChangeBtn.Location = new System.Drawing.Point(0, 40);
+            this.yaChangeBtn.Name = "yaChangeBtn";
+            this.yaChangeBtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.yaChangeBtn.Size = new System.Drawing.Size(200, 40);
+            this.yaChangeBtn.TabIndex = 1;
+            this.yaChangeBtn.Text = "Change name";
+            this.yaChangeBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.yaChangeBtn.UseVisualStyleBackColor = true;
+            // 
+            // yaDeleteBtn
+            // 
+            this.yaDeleteBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.yaDeleteBtn.FlatAppearance.BorderSize = 0;
+            this.yaDeleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yaDeleteBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.yaDeleteBtn.Location = new System.Drawing.Point(0, 0);
+            this.yaDeleteBtn.Name = "yaDeleteBtn";
+            this.yaDeleteBtn.Padding = new System.Windows.Forms.Padding(35, 0, 0, 0);
+            this.yaDeleteBtn.Size = new System.Drawing.Size(200, 40);
+            this.yaDeleteBtn.TabIndex = 0;
+            this.yaDeleteBtn.Text = "Delete";
+            this.yaDeleteBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.yaDeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // yaDiskBtn
+            // 
+            this.yaDiskBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.yaDiskBtn.FlatAppearance.BorderSize = 0;
+            this.yaDiskBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(20)))), ((int)(((byte)(0)))));
+            this.yaDiskBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yaDiskBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.yaDiskBtn.Location = new System.Drawing.Point(0, 145);
+            this.yaDiskBtn.Name = "yaDiskBtn";
+            this.yaDiskBtn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.yaDiskBtn.Size = new System.Drawing.Size(200, 45);
+            this.yaDiskBtn.TabIndex = 2;
+            this.yaDiskBtn.TabStop = false;
+            this.yaDiskBtn.Text = "Yandex Disk";
+            this.yaDiskBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.yaDiskBtn.UseVisualStyleBackColor = true;
+            this.yaDiskBtn.Click += new System.EventHandler(this.yaDiskBtn_Click);
+            // 
+            // addNewBtn
+            // 
+            this.addNewBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.addNewBtn.FlatAppearance.BorderSize = 0;
+            this.addNewBtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(20)))), ((int)(((byte)(0)))));
+            this.addNewBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addNewBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.addNewBtn.Location = new System.Drawing.Point(0, 100);
+            this.addNewBtn.Name = "addNewBtn";
+            this.addNewBtn.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.addNewBtn.Size = new System.Drawing.Size(200, 45);
+            this.addNewBtn.TabIndex = 1;
+            this.addNewBtn.TabStop = false;
+            this.addNewBtn.Text = "Add new";
+            this.addNewBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.addNewBtn.UseVisualStyleBackColor = true;
+            this.addNewBtn.Click += new System.EventHandler(this.addNewBtn_Click);
+            // 
+            // logoPanel
+            // 
+            this.logoPanel.Controls.Add(this.label1);
+            this.logoPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.logoPanel.Location = new System.Drawing.Point(0, 0);
+            this.logoPanel.Name = "logoPanel";
+            this.logoPanel.Size = new System.Drawing.Size(200, 100);
+            this.logoPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 15, 0, 0);
+            this.label1.Size = new System.Drawing.Size(197, 77);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nodoby";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // controlPanel
+            // 
+            this.controlPanel.AutoScroll = true;
+            this.controlPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(21)))), ((int)(((byte)(32)))));
+            this.controlPanel.Controls.Add(this.addDiskSelectPanel);
+            this.controlPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.controlPanel.Location = new System.Drawing.Point(200, 0);
+            this.controlPanel.Name = "controlPanel";
+            this.controlPanel.Size = new System.Drawing.Size(784, 761);
+            this.controlPanel.TabIndex = 1;
+            // 
+            // addDiskSelectPanel
+            // 
+            this.addDiskSelectPanel.AutoScroll = true;
+            this.addDiskSelectPanel.Controls.Add(this.addYaDiskBtn);
+            this.addDiskSelectPanel.Controls.Add(this.addDropboxBtn);
+            this.addDiskSelectPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addDiskSelectPanel.Location = new System.Drawing.Point(0, 0);
+            this.addDiskSelectPanel.Name = "addDiskSelectPanel";
+            this.addDiskSelectPanel.Padding = new System.Windows.Forms.Padding(25, 25, 0, 0);
+            this.addDiskSelectPanel.Size = new System.Drawing.Size(784, 761);
+            this.addDiskSelectPanel.TabIndex = 0;
+            this.addDiskSelectPanel.Visible = false;
+            // 
+            // addYaDiskBtn
+            // 
+            this.addYaDiskBtn.FlatAppearance.BorderSize = 0;
+            this.addYaDiskBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addYaDiskBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.addYaDiskBtn.Image = global::CloudDisksAggregator.Properties.Resources.yandexDiskLogo;
+            this.addYaDiskBtn.Location = new System.Drawing.Point(35, 35);
+            this.addYaDiskBtn.Margin = new System.Windows.Forms.Padding(10);
+            this.addYaDiskBtn.Name = "addYaDiskBtn";
+            this.addYaDiskBtn.Size = new System.Drawing.Size(200, 190);
+            this.addYaDiskBtn.TabIndex = 0;
+            this.addYaDiskBtn.TabStop = false;
+            this.addYaDiskBtn.Text = "Yandex Dsik";
+            this.addYaDiskBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.addYaDiskBtn.UseVisualStyleBackColor = true;
+            this.addYaDiskBtn.Click += new System.EventHandler(this.AddYandexDisk);
+            // 
+            // addDropboxBtn
+            // 
+            this.addDropboxBtn.FlatAppearance.BorderSize = 0;
+            this.addDropboxBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addDropboxBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.addDropboxBtn.Image = global::CloudDisksAggregator.Properties.Resources.dropboxlogo;
+            this.addDropboxBtn.Location = new System.Drawing.Point(255, 35);
+            this.addDropboxBtn.Margin = new System.Windows.Forms.Padding(10);
+            this.addDropboxBtn.Name = "addDropboxBtn";
+            this.addDropboxBtn.Size = new System.Drawing.Size(200, 190);
+            this.addDropboxBtn.TabIndex = 1;
+            this.addDropboxBtn.TabStop = false;
+            this.addDropboxBtn.Text = "Dropbox";
+            this.addDropboxBtn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.addDropboxBtn.UseVisualStyleBackColor = true;
+            this.addDropboxBtn.Click += new System.EventHandler(this.AddDropbox);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.controlPanel);
+            this.Controls.Add(this.sideMenuPanel);
+            this.MinimumSize = new System.Drawing.Size(950, 600);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.sideMenuPanel.ResumeLayout(false);
+            this.dropOptionPanel.ResumeLayout(false);
+            this.yaOptionPanel.ResumeLayout(false);
+            this.logoPanel.ResumeLayout(false);
+            this.logoPanel.PerformLayout();
+            this.controlPanel.ResumeLayout(false);
+            this.addDiskSelectPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel sideMenuPanel;
+        private System.Windows.Forms.Panel dropOptionPanel;
+        private System.Windows.Forms.Button dropChangeBtn;
+        private System.Windows.Forms.Button dropDeleteBtn;
+        private System.Windows.Forms.Button dropBtn;
+        private System.Windows.Forms.Panel yaOptionPanel;
+        private System.Windows.Forms.Button yaChangeBtn;
+        private System.Windows.Forms.Button yaDeleteBtn;
+        private System.Windows.Forms.Button yaDiskBtn;
+        private System.Windows.Forms.Button addNewBtn;
+        private System.Windows.Forms.Panel logoPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button helpBtn;
+        private System.Windows.Forms.Panel controlPanel;
+        private System.Windows.Forms.FlowLayoutPanel addDiskSelectPanel;
+        private System.Windows.Forms.Button addYaDiskBtn;
+        private System.Windows.Forms.Button addDropboxBtn;
     }
 }
--- CloudDisksAggregator/MainForm.cs
+++ CloudDisksAggregator/MainForm.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using CloudDisksAggregator.AuthAppData;
+using CloudDisksAggregator.ViewControls;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CloudDisksAggregator
@@ -15,6 +11,74 @@ namespace CloudDisksAggregator
         public MainForm()
         {
             InitializeComponent();
+            InitView();
         }
+
+        #region View
+        private Control[] optionPanels;
+
+        private void InitView()
+        {
+            optionPanels = new[] { yaOptionPanel, dropOptionPanel, addDiskSelectPanel };
+        }
+
+        private void HideAllPanels()
+        {
+            HideAuthControl();
+            foreach (var panel in optionPanels)
+                panel.Hide();
+        }
+
+        private void yaDiskBtn_Click(object sender, EventArgs e)
+        {
+            HideAllPanels();
+            yaOptionPanel.Show();
+        }
+
+        private void dropBtn_Click(object sender, EventArgs e)
+        {
+            HideAllPanels();
+            dropOptionPanel.Show();
+        }
+
+        private void addNewBtn_Click(object sender, EventArgs e)
+        {
+            HideAllPanels();
+            addDiskSelectPanel.Show();
+        }
+        #endregion
+
+        #region Auth
+        private void TakeToken(UserControl userControl, string token)
+        {
+            userControl.Dispose();
+        }
+
+        private void AddYandexDisk(object sender, EventArgs e)
+        {
+            HideAllPanels();
+            var authControl = new AuthCloudControl(new YandexAuthData());
+            authControl.AuthSucceeded += TakeToken;
+            controlPanel.Controls.Add(authControl);
+        }
+
+        private void AddDropbox(object sender, EventArgs e)
+        {
+            HideAllPanels();
+            var authControl = new AuthCloudControl(new DropboxAuthData());
+            authControl.AuthSucceeded += TakeToken;
+            controlPanel.Controls.Add(authControl);
+        }
+
+        private void HideAuthControl()
+        {
+            if (controlPanel.Controls.ContainsKey("AuthCloudControl"))
+            {
+                var authControl = controlPanel.Controls.Find("AuthCloudControl", false).First();
+                authControl.Dispose();
+                controlPanel.Controls.Remove(authControl);
+            }
+        }
+        #endregion
     }
 }
