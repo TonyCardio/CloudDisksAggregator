4effffd3e4cbab7984e3a694511b09e1894d7302
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2021-04-21 00:14:33 +0500

    fix Resources reference

--- CloudDisksAggregatorUI/Properties/Resources.Designer.cs
+++ CloudDisksAggregatorUI/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace CloudDisksAggregator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CloudDisksAggregator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CloudDisksAggregatorUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
