6bc50e6fdfbb57a97c536871e3ff7c6caa9654ea
BeFunny1 <BeFunny1@users.noreply.github.com>
2021-04-22 02:28:07 +0500

    build DI

--- CloudDisksAggregatorUI/Program.cs
+++ CloudDisksAggregatorUI/Program.cs
@@ -1,5 +1,11 @@
 using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
+using Autofac;
+using CloudDisksAggregator;
+using CloudDisksAggregatorInfrastructure;
 using CloudDisksAggregatorUI.UI;
 
 namespace CloudDisksAggregatorUI
@@ -16,6 +22,20 @@ namespace CloudDisksAggregatorUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+            var container = BuildContainer();
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            foreach (var assembly in Directory.GetFiles(
+                path, "CloudDisksAggregator*Module.dll").Select(Assembly.LoadFrom))
+            {
+                builder.RegisterAssemblyModules(assembly);
+            }
+
+            return builder.Build();
         }
     }
 }
