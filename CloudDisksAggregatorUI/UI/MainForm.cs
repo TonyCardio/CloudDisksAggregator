5669b86191d4b83065b24c2069c2fb2ed9ebfb37
Ilya_Shumilin <i.sh-vk@yandex.ru>
2021-04-21 23:54:39 +0500

    feature:     - add cloud selector     - add cloud content view     - add repository integration

--- CloudDisksAggregatorUI/UI/MainForm.cs
+++ CloudDisksAggregatorUI/UI/MainForm.cs
@@ -1,17 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using CloudDisksAggregatorUI.AuthAppData;
+using CloudDisksAggregator.Clouds;
+using CloudDisksAggregator.CloudWrappers;
+using CloudDisksAggregatorInfrastructure.InMemoryStorage;
+using CloudDisksAggregatorUI.Infrastructure;
 using CloudDisksAggregatorUI.UI.ViewControls;
+using CloudDisksAggregatorUI.UI.ViewEntity;
 
 namespace CloudDisksAggregatorUI.UI
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        private readonly ICloudWrapperSelector cloudWrapperSelector;
+        private readonly IInMemoryStorage<DriveViewInfo, ICloudDrive> repository;
+
+        public MainForm(
+            ICloudWrapperSelector cloudWrapperSelector,
+            WarmedUpCloudDriveStorageFactory memoryStorageFactory)
         {
             InitializeComponent();
             InitView();
+            repository = memoryStorageFactory.Create();
+            this.cloudWrapperSelector = cloudWrapperSelector;
         }
 
         #region View
@@ -20,25 +32,35 @@ namespace CloudDisksAggregatorUI.UI
 
         private void InitView()
         {
-            optionPanels = new[] {yaOptionPanel, dropOptionPanel, addDiskSelectPanel};
+            optionPanels = new[] { yaOptionPanel, dropOptionPanel, addDiskSelectPanel };
         }
 
         private void HideAllPanels()
         {
             HideAuthControl();
+            RemoveControlByKey("CloudContentControl");
+            RemoveControlByKey("SelectCloudControl");
             foreach (var panel in optionPanels)
                 panel.Hide();
+        }
+
+        private void ShowCloudContent(DriveViewInfo viewInfo, UserControl control)
+        {
+            controlPanel.Controls.Remove(control);
+            controlPanel.Controls.Add(new CloudContentControl(repository.Get(viewInfo)));
         }
 
         private void yaDiskBtn_Click(object sender, EventArgs e)
         {
             HideAllPanels();
+            InitCloudsSeletcControl(CloudDriveType.YandexDisk);
             yaOptionPanel.Show();
         }
 
         private void dropBtn_Click(object sender, EventArgs e)
         {
             HideAllPanels();
+            InitCloudsSeletcControl(CloudDriveType.Dropbox);
             dropOptionPanel.Show();
         }
 
@@ -48,29 +70,44 @@ namespace CloudDisksAggregatorUI.UI
             addDiskSelectPanel.Show();
         }
 
+        private void InitCloudsSeletcControl(CloudDriveType driveType)
+        {
+            var selectContol = new SelectCloudControl(
+                repository.GetAll()
+                .Select(pair => pair.Item1)
+                .Where(info => info.DriveType == driveType));
+            selectContol.AcceptUserChoice += ShowCloudContent;
+            controlPanel.Controls.Add(selectContol);
+        }
         #endregion
 
         #region Auth
 
-        private void TakeToken(UserControl userControl, string token)
+        private void SaveDrive(ICloudDrive cloudDrive, DriveViewInfo driveViewInfo)
         {
-            userControl.Dispose();
+            repository.Add(driveViewInfo, cloudDrive);
+            HideAllPanels();
         }
 
         private void AddYandexDisk(object sender, EventArgs e)
         {
-            HideAllPanels();
-            var authControl = new AuthCloudControl(new YandexAuthData());
-            authControl.AuthSucceeded += TakeToken;
-            controlPanel.Controls.Add(authControl);
+            InitAuthContorl(CloudDriveType.YandexDisk);
         }
 
         private void AddDropbox(object sender, EventArgs e)
         {
+            InitAuthContorl(CloudDriveType.Dropbox);
+        }
+
+        private void InitAuthContorl(CloudDriveType driveType)
+        {
             HideAllPanels();
-            var authControl = new AuthCloudControl(new DropboxAuthData());
-            authControl.AuthSucceeded += TakeToken;
-            controlPanel.Controls.Add(authControl);
+            if (cloudWrapperSelector.TryGetCloudWrapper(driveType, out var driveWrapper))
+            {
+                var authControl = new AuthCloudControl(driveWrapper);
+                authControl.AuthSucceeded += SaveDrive;
+                controlPanel.Controls.Add(authControl);
+            }
         }
 
         private void HideAuthControl()
@@ -84,5 +121,11 @@ namespace CloudDisksAggregatorUI.UI
         }
 
         #endregion
+
+        private void RemoveControlByKey(string key)
+        {
+            if (controlPanel.Controls.ContainsKey(key))
+                controlPanel.Controls.RemoveByKey(key);
+        }
     }
 }
---
+++ CloudDisksAggregatorUI/UI/ViewControls/CloudContentControl.cs
@@ -0,0 +1,45 @@
+﻿using CloudDisksAggregator.Clouds;
+using CloudDisksAggregator.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudDisksAggregatorUI.UI.ViewControls
+{
+    public partial class CloudContentControl : UserControl
+    {
+        private readonly ICloudDrive cloudDrive;
+
+        public CloudContentControl(ICloudDrive cloudDrive)
+        {
+            Dock = DockStyle.Fill;
+            this.cloudDrive = cloudDrive;
+            AddItems();
+            InitializeComponent();
+            Name = "CloudContentControl";
+        }
+
+        public async void AddItems()
+        {
+            var items = await cloudDrive.GetCatalogContents();
+            viewContentList.Items.AddRange(items.Select(CreateViewItem).ToArray());
+        }
+
+        private ListViewItem CreateViewItem(DiskEntityInfo diskEntity)
+        {
+            var item = new ListViewItem(
+                new string[] { diskEntity.Name },
+                diskEntity.Expansion == "Dir" ? 1 : 0,
+                SystemColors.ButtonFace, Color.Empty,
+              new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point));
+            item.Tag = diskEntity;
+            return item;
+        }
+    }
+}
