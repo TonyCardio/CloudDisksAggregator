16c70c0be3b74e8ea3ae748abfeed194d3b3746e
Anton Nechugovskikh <nechugovskihanton@gmail.com>
2021-04-21 15:02:48 +0500

    feature     add IInMemoryStorage interface     add SimpleInMemoryStorage implementation

---
+++ CloudDisksAggregatorInfrastructure/InMemoryStorage/IInMemoryStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace CloudDisksAggregatorInfrastructure.InMemoryStorage
+{
+    public interface IInMemoryStorage<TKey, TValue>
+    {
+        TValue Get(TKey key);
+        void Add(TKey key, TValue value);
+    }
+}
---
+++ CloudDisksAggregatorInfrastructure/InMemoryStorage/SimpleInMemoryStorage.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CloudDisksAggregatorInfrastructure.InMemoryStorage
+{
+    public class SimpleInMemoryStorage<TKey, TValue> : IInMemoryStorage<TKey, TValue>
+    {
+        private static JsonSerializerSettings settings =
+            new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+        private readonly Dictionary<TKey, TValue> items;
+        private readonly string pathToData;
+
+        public SimpleInMemoryStorage(string pathToData)
+        {
+            this.pathToData = pathToData;
+            if (!Directory.Exists(pathToData))
+                Directory.CreateDirectory(pathToData);
+            items = new Dictionary<TKey, TValue>();
+        }
+
+        public TValue Get(TKey key)
+        {
+            return items[key];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            items.Add(key, value);
+            var kvp = new KeyValuePair<TKey, TValue>(key, value);
+            var serialized = JsonConvert.SerializeObject(kvp, Formatting.Indented, settings);
+            File.WriteAllText(Path.Join(pathToData, $"{key}.json"), serialized);
+        }
+
+        public void WarmUpCache()
+        {
+            foreach (var filePath in Directory.GetFiles(pathToData))
+            {
+                var serialized = File.ReadAllText(filePath);
+                var (key, value) = JsonConvert.DeserializeObject<KeyValuePair<TKey, TValue>>(serialized, settings);
+                items.Add(key, value);
+            }
+        }
+    }
+}
