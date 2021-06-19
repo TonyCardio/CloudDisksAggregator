aabd585c1d8b6c77ffa4b717e8a91780cd4c42fc
BeFunny1 <BeFunny1@users.noreply.github.com>
2021-06-20 04:12:05 +0500

    add class for search files on drive

---
+++ CloudDisksAggregator/Core/EntityFinder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CloudDisksAggregator.Core
+{
+    public static class EntityFinder
+    {
+        public static List<DriveEntityInfo> Search(string searchLine, IEnumerable<DriveEntityInfo> allEntity) 
+            => allEntity.Where(entityInfo => entityInfo.Name.Contains(searchLine)).ToList();
+    }
+}
