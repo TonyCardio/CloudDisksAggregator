107b8cd5cd5b94bda8f5f08c8e5bdfb253e6f7f3
BeFunny1 <BeFunny1@users.noreply.github.com>
2021-06-22 20:48:34 +0500

    add comment with suggestions

--- CloudDisksAggregator/Core/EntityFinder.cs
+++ CloudDisksAggregator/Core/EntityFinder.cs
@@ -5,6 +5,9 @@ namespace CloudDisksAggregator.Core
 {
     public static class EntityFinder
     {
+        // It is assumed that an efficient file search method will be implemented here,
+        // or a library that does this will be added.
+        //      Best wishes from the CloudDiskAggregator team.
         public static List<DriveEntityInfo> Search(string searchLine, IEnumerable<DriveEntityInfo> allEntity) 
             => allEntity.Where(entityInfo => entityInfo.Name.Contains(searchLine)).ToList();
     }
