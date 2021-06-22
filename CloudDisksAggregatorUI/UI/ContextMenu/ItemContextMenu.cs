a563746fbc44b233ee8ee4053ec3cfb758a56c2f
Ilya_Shumilin <i.sh-vk@yandex.ru>
2021-06-23 03:29:19 +0500

    reature:     - add menu click handlers

--- CloudDisksAggregatorUI/UI/ContextMenu/ItemContextMenu.cs
+++ CloudDisksAggregatorUI/UI/ContextMenu/ItemContextMenu.cs
@@ -9,9 +9,8 @@ namespace CloudDisksAggregatorUI.UI.ContextMenu
     public class ItemContextMenu : ContextMenuStrip
     {
         public event Action<string, DriveEntityInfo> OnRename;
-        public event Action OnGetLink;
         public event Action<DriveEntityInfo> OnDelete;
-        public event Action<DriveEntityInfo> OnDownload;
+        public event Action<string, DriveEntityInfo> OnDownload;
 
         private DriveEntityInfo item;
 
@@ -19,7 +18,6 @@ namespace CloudDisksAggregatorUI.UI.ContextMenu
 
         private readonly ToolStripMenuItem deleteItem = new ToolStripMenuItem("Delete");
         private readonly ToolStripMenuItem renameItem = new ToolStripMenuItem("Rename");
-        private readonly ToolStripMenuItem getLinkItem = new ToolStripMenuItem("Get link");
         private readonly ToolStripMenuItem downloadItem = new ToolStripMenuItem("Download");
         private readonly ToolStripTextBox newNameBox = new ToolStripTextBox()
         {
@@ -34,7 +32,7 @@ namespace CloudDisksAggregatorUI.UI.ContextMenu
             BackColor = Color.FromArgb(35, 32, 39);
             RenderMode = ToolStripRenderMode.System;
             ForeColor = SystemColors.ButtonHighlight;
-            Items.AddRange(new ToolStripItem[] { deleteItem, downloadItem, getLinkItem, renameItem, newNameBox });
+            Items.AddRange(new ToolStripItem[] { deleteItem, downloadItem, renameItem, newNameBox });
             InitItems();
         }
 
@@ -50,10 +48,18 @@ namespace CloudDisksAggregatorUI.UI.ContextMenu
         {
             foreach (ToolStripItem toolStripItem in Items) toolStripItem.ForeColor = SystemColors.ButtonHighlight;
             deleteItem.Click += (s, e) => OnDelete?.Invoke(item);
-            getLinkItem.Click += (s, e) => OnGetLink?.Invoke();
-            downloadItem.Click += (s, e) => OnDownload?.Invoke(item);
+            downloadItem.Click += DownloadItem_Click;
             renameItem.Click += RenameItem_Click;
             newNameBox.KeyPress += NewNameBox_KeyPress;
+        }
+
+        private void DownloadItem_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                OnDownload?.Invoke(dialog.SelectedPath, item);
+            }
         }
 
         private void NewNameBox_KeyPress(object sender, KeyPressEventArgs e)
@@ -62,6 +68,7 @@ namespace CloudDisksAggregatorUI.UI.ContextMenu
             {
                 Items.Remove(newNameBox);
                 OnRename?.Invoke($"{newNameBox.Text}.{item.Expansion}", item);
+                Hide();
             }
         }
 
